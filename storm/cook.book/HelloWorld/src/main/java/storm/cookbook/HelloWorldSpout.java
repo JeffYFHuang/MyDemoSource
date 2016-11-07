97250c79779db5ab10e51361f67bc7653647d00f
JeffYFHuang <JeffYFHuang@github.com>
2016-11-07 12:26:19 +0800

    compile pass with storm 1.0.2!

--- storm/cook.book/HelloWorld/src/main/java/storm/cookbook/HelloWorldSpout.java
+++ storm/cook.book/HelloWorld/src/main/java/storm/cookbook/HelloWorldSpout.java
@@ -3,45 +3,45 @@ package storm.cookbook;
 import java.util.Map;
 import java.util.Random;
 
-import backtype.storm.spout.SpoutOutputCollector;
-import backtype.storm.task.TopologyContext;
-import backtype.storm.topology.OutputFieldsDeclarer;
-import backtype.storm.topology.base.BaseRichSpout;
-import backtype.storm.tuple.Fields;
-import backtype.storm.tuple.Values;
-import backtype.storm.utils.Utils;
+import org.apache.storm.spout.SpoutOutputCollector;
+import org.apache.storm.task.TopologyContext;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.base.BaseRichSpout;
+import org.apache.storm.tuple.Fields;
+import org.apache.storm.tuple.Values;
+import org.apache.storm.utils.Utils;
 
 public class HelloWorldSpout extends BaseRichSpout {
+    private SpoutOutputCollector collector;
+    private int referenceRandom;
+    private static final int MAX_RANDOM = 10;
+    public HelloWorldSpout(){
+    final Random rand = new Random();
+    referenceRandom = rand.nextInt(MAX_RANDOM);
+    }
+    
 
-	private SpoutOutputCollector collector;
-	private int referenceRandom;
-	private static final int MAX_RANDOM = 10;
-	public HelloWorldSpout(){
-	final Random rand = new Random();
-	referenceRandom = rand.nextInt(MAX_RANDOM);
-	}
-	
-	@Override
-	public void open(Map conf, TopologyContext context, SpoutOutputCollector collector) {
-		this.collector = collector;
-	}
+    @Override
+    public void open(Map conf, TopologyContext context, SpoutOutputCollector collector) {
+            this.collector = collector;
+    }
 
-	@Override
-	public void nextTuple() {
-		Utils.sleep(100);
-		final Random rand = new Random();
-		int instanceRandom = rand.nextInt(MAX_RANDOM);
-		if(instanceRandom == referenceRandom){
-		collector.emit(new Values("Hello World"));
-		} else {
-		collector.emit(new Values("Other Random Word"));
-		}
+    @Override
+    public void nextTuple() {
+            Utils.sleep(100);
+            final Random rand = new Random();
+            int instanceRandom = rand.nextInt(MAX_RANDOM);
+            if(instanceRandom == referenceRandom){
+            collector.emit(new Values("Hello World"));
+            } else {
+            collector.emit(new Values("Other Random Word"));
+            }
 
-	}
+    }
 
-	@Override
-	public void declareOutputFields(OutputFieldsDeclarer declarer) {
-		declarer.declare(new Fields("sentence"));
-	}
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+            declarer.declare(new Fields("sentence"));
+    }
 
 }
--- storm/cook.book/HelloWorld/src/main/java/storm/cookbook/HelloWorldTopology.java
+++ storm/cook.book/HelloWorld/src/main/java/storm/cookbook/HelloWorldTopology.java
@@ -1,37 +1,38 @@
 package storm.cookbook;
 
-import backtype.storm.Config;
-import backtype.storm.LocalCluster;
-import backtype.storm.StormSubmitter;
-import backtype.storm.generated.AlreadyAliveException;
-import backtype.storm.generated.InvalidTopologyException;
-import backtype.storm.topology.TopologyBuilder;
-import backtype.storm.utils.Utils;
+import org.apache.storm.Config;
+import org.apache.storm.LocalCluster;
+import org.apache.storm.StormSubmitter;
+import org.apache.storm.generated.AlreadyAliveException;
+import org.apache.storm.generated.InvalidTopologyException;
+import org.apache.storm.generated.AuthorizationException;
+import org.apache.storm.topology.TopologyBuilder;
+import org.apache.storm.utils.Utils;
 
 public class HelloWorldTopology {
-	public static void main(String args[]) throws AlreadyAliveException, InvalidTopologyException {
-		TopologyBuilder builder = new TopologyBuilder();
-		builder.setSpout("randomHelloWorld", new
-		HelloWorldSpout(), 10);
-		builder.setBolt("HelloWorldBolt", new
-		               HelloWorldBolt(), 2)
-		.shuffleGrouping("randomHelloWorld");
-		
-		Config conf = new Config();
-		conf.setDebug(true);
-		
-		if(args!=null && args.length > 0) {
-	   	   conf.setNumWorkers(3);
-	   	   
-		   StormSubmitter.submitTopology(args[0], conf,
-		                                builder.createTopology());
-		} else {
-		   LocalCluster cluster = new LocalCluster();
-		   cluster.submitTopology("test", conf,
-		                         builder.createTopology());
-		   Utils.sleep(10000);
-		   cluster.killTopology("test");
-		   cluster.shutdown();
-		}
+        public static void main(String args[]) throws AlreadyAliveException, InvalidTopologyException, AuthorizationException {
+                TopologyBuilder builder = new TopologyBuilder();
+                builder.setSpout("randomHelloWorld", new
+                HelloWorldSpout(), 10);
+                builder.setBolt("HelloWorldBolt", new
+                               HelloWorldBolt(), 2)
+                .shuffleGrouping("randomHelloWorld");
+
+                Config conf = new Config();
+                conf.setDebug(true);
+
+                if(args!=null && args.length > 0) {
+                   conf.setNumWorkers(3);
+
+                   StormSubmitter.submitTopology(args[0], conf,
+                                                builder.createTopology());
+                } else {
+                   LocalCluster cluster = new LocalCluster();
+                   cluster.submitTopology("test", conf,
+                                         builder.createTopology());
+                   Utils.sleep(10000);
+                   cluster.killTopology("test");
+                   cluster.shutdown();
+                }
     }
 }
