a6a93f38732e7c2ab28f75d0f24234a271fc2362
JeffYFHuang <gsmcci58@github.com>
2016-11-09 16:24:46 +0800

    add multilang!

--- storm/cook.book/HelloWorld/multilang/resources/script.R
+++ storm/cook.book/HelloWorld/multilang/resources/script.R
@@ -4,9 +4,9 @@ storm = Storm$new();
 storm$lambda = function(s) { 
   t = s$tuple; 
   t$output = vector(mode="character",length=1);
-  clicks = as.numeric(t$input[1]); 
-  views = as.numeric(t$input[2]); 
-  t$output[1] = rbeta(1, clicks, views - clicks); 
+  #clicks = as.numeric(t$input[1]); 
+  #views = as.numeric(t$input[2]); 
+  t$output[1] = 10; #rbeta(1, clicks, views - clicks); 
   s$emit(t); 
   #alternative: mark the tuple as failed. 
   s$fail(t);  
