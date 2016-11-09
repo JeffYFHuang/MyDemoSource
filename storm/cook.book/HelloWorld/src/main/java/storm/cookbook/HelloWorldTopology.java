ef4e83210023b9467a70bc272a7b5dff15f9c640
JeffYFHuang <JeffYFHuang@github.com>
2016-11-09 15:12:18 +0800

    Storm Hello world with R bolt!

--- storm/cook.book/HelloWorld/src/main/java/storm/cookbook/HelloWorldTopology.java
+++ storm/cook.book/HelloWorld/src/main/java/storm/cookbook/HelloWorldTopology.java
@@ -6,16 +6,39 @@ import org.apache.storm.StormSubmitter;
 import org.apache.storm.generated.AlreadyAliveException;
 import org.apache.storm.generated.InvalidTopologyException;
 import org.apache.storm.generated.AuthorizationException;
+import org.apache.storm.task.ShellBolt;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.IRichBolt;
 import org.apache.storm.topology.TopologyBuilder;
+import org.apache.storm.tuple.Fields;
 import org.apache.storm.utils.Utils;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HelloWorldTopology {
+
+    public static class RBolt extends ShellBolt implements IRichBolt
+    {
+        public RBolt() {
+           super("Rscript", "script.R");
+        }
+        
+        @Override
+        public void declareOutputFields(OutputFieldsDeclarer declarer) {
+          declarer.declare(new Fields("word"));
+        }
+
+        @Override
+        public Map<String, Object> getComponentConfiguration() {
+          return null;
+        }
+    }
+
         public static void main(String args[]) throws AlreadyAliveException, InvalidTopologyException, AuthorizationException {
                 TopologyBuilder builder = new TopologyBuilder();
-                builder.setSpout("randomHelloWorld", new
-                HelloWorldSpout(), 10);
-                builder.setBolt("HelloWorldBolt", new
-                               HelloWorldBolt(), 2)
+                builder.setSpout("randomHelloWorld", new HelloWorldSpout(), 10);
+                builder.setBolt("HelloWorldBolt", new RBolt(), 2)
                 .shuffleGrouping("randomHelloWorld");
 
                 Config conf = new Config();
