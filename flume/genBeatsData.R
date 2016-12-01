b9a285d82f01d17da7434e780f509c710554aea1
JeffYFHuang <gsmcci58@github.com>
2016-12-01 16:28:45 +0800

    hrm original data create from physiobank apnea data!

---
+++ flume/genBeatsData.R
@@ -0,0 +1,27 @@
+#!/usr/bin/env Rscript
+require(RHRV)
+require(rjson)
+
+recordPath = "/media/Data/wfdbData/apnea-ecg/"
+recordNames = list.files(path=recordPath, pattern=".qrs")
+recordNames = unlist(strsplit(recordNames, '.qrs'))
+
+for (recordName in recordNames) {
+    data = list()
+    hrv.wfdb = CreateHRVData()
+    hrv.wfdb = SetVerbose(hrv.wfdb, FALSE)
+
+    hrv.wfdb = LoadBeatWFDB(hrv.wfdb, recordName, recordPath, annotator = "qrs")
+    Beat = hrv.wfdb$Beat$Time
+    TailBeat = tail(Beat, 1)
+    data$Beat = c(Beat, TailBeat + Beat[Beat < 43200 - TailBeat])
+    
+    if (file.exists(paste(recordPath, recordName, ".apn", sep=""))) {
+       hrv.wfdb = LoadApneaWFDB(hrv.wfdb, recordName, Tag="Apnea", recordPath)
+       a = hrv.wfdb$Episodes
+       b = a
+       b[,1] = b[,1]+TailBeat
+       data$Episodes = rbind(a, b)
+    }
+    cat(recordName, "\t", toJSON(data), "\n", sep="")
+}
