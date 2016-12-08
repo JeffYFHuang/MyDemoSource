555bc986a28912b76828f4c3dbfac988ba4d7d6a
JeffYFHuang <JeffYFHuang@github.com>
2016-12-08 15:42:58 +0800

    add arguments!

--- flume/hdfs_send_beats.R
+++ flume/hdfs_send_beats.R
@@ -1,6 +1,6 @@
 library(RCurl)
 library(rjson)
-library(parallel)
+#library(parallel)
 
 send_one_data <- function (data, subject = "x01", addr="172.18.161.100", port=44448) {
     payload = data
@@ -22,21 +22,43 @@ generateHRV <- function(hrvdata, reptimes = 144, replace=T) {
     hrvdata # <- lapply(hrvdata, toJSON)
 }
 
-flumeserver = "172.18.161.1"
-port = 44448
+args=(commandArgs(TRUE))
+ip = NULL
+port = NULL
+num = NULL
+args=(commandArgs(TRUE))
+
+if (length(args) != 0){
+  for (i in 1:length(args)){
+     eval(parse(text=args[[i]]))
+  }
+}
+
+execution = "Exe. Rscript hdfs_send_beats.R \"ip='10.0.0.1'\" port=44448 num=1000";
+if (is.null(ip))
+   stop(paste("please provide flume server address!", execution))
+if (is.null(port))
+   stop(paste("please provide port of flume service!", execution))
+if (is.null(port))
+   stop(paste("please provide the number of messages to send!", execution))
+
+flumeserver = ip
+nodename = Sys.info()["nodename"]
+pid = Sys.getpid()
+
 recordPath = "hrmdata"
+
 conn = file.path(recordPath, "beatsdata.txt")
-
 lines <-readLines(conn)
-
 curSubject <- ""
 hrvdata <- NULL
 count = 1
-while (count < 10000) {
+while (count <= num) {
     data <- sample(lines, 1)
     val <- unlist(strsplit(data, "\t"))
-    send_one_data(val[2], subject = paste(val[1], "-", count, sep=""), addr=flumeserver, port=port) 
+    subject = paste(val[1], "-", nodename, "-", pid, "-", count, sep="")
+    send_one_data(val[2], subject, addr=flumeserver, port=port)
    #lapply (hrvdata, send_one_hrv, subject = paste(curSubject, "-YFnb1", count, sep=""), addr=flumeserver, port=port)
-    print(paste(val[1], "-", count, sep=""))
+    print(subject)
     count = count + 1
 }
