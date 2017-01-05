6a2df73145ef2cd40f5c9e8e0dc644e983435fa7
JeffYFHuang <gsmcci58@github.com>
2017-01-05 17:32:47 +0800

    add hybird kafka consumer!

--- flume/hrv_simulation/http_send_beats_kafka.R
+++ flume/hrv_simulation/http_send_beats_kafka.R
@@ -1,24 +1,64 @@
 library(RCurl)
+library(rjson)
+#library(parallel)
 
-send_beats <- function (subject = "x01", path = "xbeats",  addr="172.18.161.100", port=44448, iter = i) {
-    file = file.path(path, paste(subject, ".beats", sep=""))
-    beats = readLines(file)
-    payload = paste(beats, collapse=" ")
-    headers = format(Sys.time(), paste('"headers":{"m_user":"subject_', iter , '","m_year":"%Y","m_month":"%m","m_day":"%d", "m_hour":"%H", "key":"', iter, '", "topic": "beats-', iter%%4, '"}', sep=""))
+send_one_data <- function (data, subject = "x01", addr="172.18.161.100", port=44448) {
+    payload = data
+    headers = format(Sys.time(), paste('"headers":{"key":"', count, '", "topic": "hrvs"}', sep=""))
 
-    http_content = paste('[{', headers, ',"body":"subject_jeffnb_', iter, "\t", payload, '"}]', sep="")
+    http_content = paste('[{', headers, ',"body":', "'", subject, '\t', payload, "'", '}]', sep="")
     httpheader <- c(Accept="application/json; charset=UTF-8", "Content-Type"="application/json")
     postForm(paste("http://", addr, ":", port, sep=""), .opts=list(httpheader=httpheader, postfields=http_content))
 }
 
-flumeserver = "172.18.161.1"
-port = 44448
-recordPath = "beats"
-recordNames = list.files(path=recordPath, pattern=".beats")
-recordNames = unlist(strsplit(recordNames, '.beats'))
-#for (i in 1:900) {
-for (subject in recordNames) {
-    print(paste(subject, "_", i, sep=""))
-    send_beats(subject, addr=flumeserver, port=port, path=recordPath, iter = (i-1)*length(recordNames) + which(recordNames==subject))
+generateHRV <- function(hrvdata, reptimes = 144, replace=T) {
+    hrvdata <- sample(hrvdata, reptimes, replace=T)
+    hrvdata <- lapply(hrvdata, fromJSON)
+    for (i in 1:length(hrvdata)){
+        hrvdata[[i]]$startTime <-  times[i]
+        hrvdata[[i]]$endTime <- times[i] + 300
+    }
+
+    hrvdata # <- lapply(hrvdata, toJSON)
 }
-#}
+
+args=(commandArgs(TRUE))
+ip = NULL
+port = NULL
+num = NULL
+args=(commandArgs(TRUE))
+
+if (length(args) != 0){
+  for (i in 1:length(args)){
+     eval(parse(text=args[[i]]))
+  }
+}
+
+execution = "Exe. Rscript http_send_beats_kafka.R \"ip='10.0.0.5'\" port=44448 num=1";
+if (is.null(ip))
+   stop(paste("please provide flume server address!", execution))
+if (is.null(port))
+   stop(paste("please provide port of flume service!", execution))
+if (is.null(port))
+   stop(paste("please provide the number of messages to send!", execution))
+
+flumeserver = ip
+nodename = Sys.info()["nodename"]
+pid = Sys.getpid()
+
+recordPath = "../hrmdata"
+
+conn = file.path(recordPath, "beatsdata.txt")
+lines <-readLines(conn)
+curSubject <- ""
+hrvdata <- NULL
+count = 1
+while (count <= num) {
+    data <- sample(lines, 1)
+    val <- unlist(strsplit(data, "\t"))
+    subject = paste(val[1], "-", nodename, "-", pid, "-", count, sep="")
+    send_one_data(val[2], subject, addr=flumeserver, port=port)
+    #lapply (hrvdata, send_one_hrv, subject = paste(curSubject, "-YFnb1", count, sep=""), addr=flumeserver, port=port)
+    print(subject)
+    count = count + 1
+}
