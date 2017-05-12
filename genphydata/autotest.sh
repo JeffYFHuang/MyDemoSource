d14747a27f12662d227ed7c74bb0fd406c79bea2
JeffYFHuang <JeffYFHuang@github.com>
2017-05-12 17:31:11 +0800

    Change loops of auto-running.

--- genphydata/autotest.sh
+++ genphydata/autotest.sh
@@ -9,34 +9,34 @@ hours=(02 08 14 20)
 phytypes=('context' 'step' 'sleep' 'hrm')
 ptypes=('date' 'week' 'month')
 
-for i in `seq 0 $daysofmonth`
+for ptype in "${ptypes[@]}"
 do
-   cdate=$(date '+%C%y-%m-%d' -d "$firstdayofmonth+$i days")
-   echo $cdate
+   for i in `seq 0 $daysofmonth`
+   do
+      cdate=$(date '+%C%y-%m-%d' -d "$firstdayofmonth+$i days")
+      echo $cdate
 # generate dummy an send to flume service to store data in hdfs.
 #   Rscript send_phydummy.R "ip='172.18.161.1'" port=44448 bdate="'$cdate'" range=1:20000 hours=24
 
-   p1=`echo $cdate | cut -d - -f 1`
-   p2=`echo $cdate | cut -d - -f 2`
-   p3=`echo $cdate | cut -d - -f 3`
+      p1=`echo $cdate | cut -d - -f 1`
+      p2=`echo $cdate | cut -d - -f 2`
+      p3=`echo $cdate | cut -d - -f 3`
 
-   path=$p1/$p2/$p3
+      path=$p1/$p2/$p3
 #   testpath=/data/physical/in/elm124614/$p1/$p2/$p3
 
-   for hour in "${hours[@]}"
-   do
-#      hadoop fs -test -d $testpath/$hour
-#      if [ $? == 0 ]; then
-         echo . ./insertPhyData.sh 6 $path/$hour/*
-         . ./insertPhyData.sh 6 $path/$hour
-#      else
-#         echo $testpath/$hour "not exists"
-#      fi
-   done
+      for hour in "${hours[@]}"
+      do
+#         hadoop fs -test -d $testpath/$hour
+#         if [ $? == 0 ]; then
+            echo . ./insertPhyData.sh 6 $path/$hour/*
+            . ./insertPhyData.sh 6 $path/$hour
+#         else
+#            echo $testpath/$hour "not exists"
+#         fi
+      done
 
-   for phytype in "${phytypes[@]}"
-   do
-      for ptype in "${ptypes[@]}"
+      for phytype in "${phytypes[@]}"
       do
           echo Rscript src/processCassPhyDateData.R date="'$cdate'" phytype="'$phytype'" ptype="'$ptype'"
           Rscript src/processCassPhyDateData.R date="'$cdate'" phytype="'$phytype'" ptype="'$ptype'"
--- genphydata/restorePhyData.sh
+++ genphydata/restorePhyData.sh
@@ -1,28 +1,28 @@
 #!/usr/bin/env bash
 
-hadoop fs -rmr /tmp/restore
-hadoop jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-2.6.0.jar -D mapreduce.job.maps=$1 -files src/ -mapper src/processPhyReducer.R -input $2 -output /tmp/restore
-hadoop fs -rmr /tmp/restore
+#hadoop fs -rmr /tmp/restore
+#hadoop jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-2.6.0.jar -D mapreduce.job.maps=$1 -files src/ -mapper src/processPhyReducer.R -input $2 -output /tmp/restore
+#hadoop fs -rmr /tmp/restore
 
 firstdayofmonth=$3
 daysofmonth=$4
 phytypes=('context' 'step' 'sleep' 'hrm')
 ptypes=('date' 'week' 'month')
 
-for i in `seq 0 $daysofmonth`
+for ptype in "${ptypes[@]}"
 do
-   cdate=$(date '+%C%y-%m-%d' -d "$firstdayofmonth+$i days")
-   echo $cdate
-
-   p1=`echo $cdate | cut -d - -f 1`
-   p2=`echo $cdate | cut -d - -f 2`
-   p3=`echo $cdate | cut -d - -f 3`
-
-   path=$p1/$p2/$p3
-
-   for phytype in "${phytypes[@]}"
+   for i in `seq 0 $daysofmonth`
    do
-      for ptype in "${ptypes[@]}"
+      cdate=$(date '+%C%y-%m-%d' -d "$firstdayofmonth+$i days")
+      echo $cdate
+
+      p1=`echo $cdate | cut -d - -f 1`
+      p2=`echo $cdate | cut -d - -f 2`
+      p3=`echo $cdate | cut -d - -f 3`
+
+      path=$p1/$p2/$p3
+
+      for phytype in "${phytypes[@]}"
       do
           echo Rscript src/processCassPhyDateData.R date="'$cdate'" phytype="'$phytype'" ptype="'$ptype'"
           Rscript src/processCassPhyDateData.R date="'$cdate'" phytype="'$phytype'" ptype="'$ptype'"
