a856ebb024fc3372ccdd60fa835da29c31df19cf
JeffYFHuang <JeffYFHuang@github.com>
2018-07-24 17:31:58 +0800

    streaming

---
+++ storm/storm-examples/examples/jpmml-storm/pmml-kafka-storm-hdfs-mqtt/src/main/java/org/jpmml/storm/Main.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright (c) 2014 Villu Ruusmann
+ *
+ * This file is part of JPMML-Storm
+ *
+ * JPMML-Storm is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JPMML-Storm is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with JPMML-Storm.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.jpmml.storm;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import org.apache.storm.Config;
+import org.apache.storm.LocalCluster;
+import org.apache.storm.StormSubmitter;
+import org.apache.storm.generated.StormTopology;
+import org.apache.storm.hdfs.bolt.HdfsBolt;
+import org.apache.storm.hdfs.bolt.format.DefaultFileNameFormat;
+import org.apache.storm.hdfs.bolt.format.DelimitedRecordFormat;
+import org.apache.storm.hdfs.bolt.format.FileNameFormat;
+import org.apache.storm.hdfs.bolt.format.RecordFormat;
+import org.apache.storm.hdfs.bolt.rotation.FileRotationPolicy;
+import org.apache.storm.hdfs.bolt.rotation.FileSizeRotationPolicy;
+import org.apache.storm.hdfs.bolt.rotation.FileSizeRotationPolicy.Units;
+import org.apache.storm.hdfs.bolt.rotation.TimedRotationPolicy;
+import org.apache.storm.hdfs.bolt.rotation.TimedRotationPolicy.TimeUnit;
+import org.apache.storm.hdfs.bolt.sync.CountSyncPolicy;
+import org.apache.storm.hdfs.bolt.sync.SyncPolicy;
+import org.apache.storm.hdfs.common.rotation.MoveFileAction;
+import org.apache.storm.kafka.KafkaSpout;
+import org.apache.storm.kafka.SpoutConfig;
+import org.apache.storm.kafka.StringScheme;
+import org.apache.storm.kafka.ZkHosts;
+import org.apache.storm.spout.SchemeAsMultiScheme;
+import org.apache.storm.topology.TopologyBuilder;
+import org.apache.storm.tuple.Fields;
+import org.dmg.pmml.FieldName;
+import org.jpmml.evaluator.Evaluator;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+/*		if(args.length != 2){
+			System.err.println("Usage: java " + Main.class.getName() + " <PMML file> <Output CSV file>");
+
+			System.exit(-1);
+		}
+*/		
+        // Configuration
+        String zookeeperHost = "localhost:2181";
+        ZkHosts zkHosts = new ZkHosts(zookeeperHost);
+
+        SpoutConfig spoutConfig = new SpoutConfig(zkHosts, "hrvs", "", "hrvstream");//UUID.randomUUID().toString());
+        spoutConfig.scheme = new SchemeAsMultiScheme(new StringScheme());
+        spoutConfig.bufferSizeBytes = 1024 * 1024 * 1024;
+        spoutConfig.fetchMaxWait = 100;
+        
+        KafkaSpout kafkaspout = new KafkaSpout(spoutConfig);
+
+		Evaluator evaluator = PMMLBoltUtil.createEvaluator(new File(args[0]));
+
+		PMMLBolt pmmlBolt = new PMMLBolt(evaluator);
+		Fields hdfsFields = new Fields("subject", "apnea");
+		// use "|" instead of "," for field delimiter
+		RecordFormat format = new DelimitedRecordFormat()
+		        .withFields(hdfsFields)
+		        .withFieldDelimiter("\t");
+
+		// sync the filesystem after every 1k tuples
+		SyncPolicy syncPolicy = new CountSyncPolicy(1000);
+
+		// rotate files when they reach 5MB
+//		FileRotationPolicy rotationPolicy = new TimedRotationPolicy(1.0f, TimeUnit.MINUTES);
+		FileRotationPolicy rotationPolicy = new FileSizeRotationPolicy(128.0f, Units.MB);
+
+		FileNameFormat fileNameFormat = new DefaultFileNameFormat()
+		        .withPath("/data/stormtest/").withPrefix("terminalInfo_").withExtension(".log");
+
+		HdfsBolt hdfsbolt = new HdfsBolt()
+		        .withFsUrl("hdfs://192.168.0.154:9000")
+		        .withFileNameFormat(fileNameFormat)
+		        .withRecordFormat(format)
+		        .withRotationPolicy(rotationPolicy)
+		        .withSyncPolicy(syncPolicy)
+		        .addRotationAction(new MoveFileAction().toDestination("/data/stormtest/"));
+
+/*		List<FieldName> outputFields = new ArrayList<>();
+		outputFields.add(new FieldName("subject"));
+		outputFields.add(new FieldName("apnea"));
+//		outputFields.addAll(evaluator.getTargetFields());
+//		outputFields.addAll(evaluator.getOutputFields());
+		CsvWriterBolt csvWriter = new CsvWriterBolt(new File(args[1]), outputFields);
+*/
+		TopologyBuilder topologyBuilder = new TopologyBuilder();
+		topologyBuilder.setSpout("input", kafkaspout);
+		topologyBuilder.setBolt("pmml", pmmlBolt, 3)
+			.shuffleGrouping("input");
+		//topologyBuilder.setBolt("csvWriter", csvWriter, 2)
+		//.shuffleGrouping("pmml");
+		topologyBuilder.setBolt("hdfsbolt", hdfsbolt, 3)
+			.shuffleGrouping("pmml");
+
+		Config config = new Config();
+		config.setDebug(true);
+        //config.put(Config.STORM_BLOBSTORE_INPUTSTREAM_BUFFER_SIZE_BYTES, 655360);
+        config.put(Config.TOPOLOGY_MESSAGE_TIMEOUT_SECS, 3600);
+        config.put(Config.NIMBUS_THRIFT_MAX_BUFFER_SIZE, 25384000);
+ 
+		StormTopology topology = topologyBuilder.createTopology();
+		
+        if (args != null && args.length > 1) {
+	           config.setNumWorkers(8);
+
+	           StormSubmitter.submitTopologyWithProgressBar(args[1], config, topology);
+	    } else {
+	           LocalCluster cluster = new LocalCluster();
+	           cluster.submitTopology("prediction", new Config(), topology);
+	           Thread.sleep(600 * 1000);
+	           cluster.killTopology("prediction");
+
+	           cluster.shutdown();
+	    }
+    }
+}
---
+++ storm/storm-examples/examples/jpmml-storm/pmml-kafka-storm-hdfs-mqtt/src/main/java/org/jpmml/storm/PMMLBolt.java
@@ -0,0 +1,247 @@
+/*
+ * Copyright (c) 2014 Villu Ruusmann
+ *
+ * This file is part of JPMML-Storm
+ *
+ * JPMML-Storm is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JPMML-Storm is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with JPMML-Storm.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.jpmml.storm;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.apache.storm.shade.org.json.simple.JSONArray;
+import org.apache.storm.shade.org.json.simple.JSONObject;
+import org.apache.storm.shade.org.json.simple.parser.JSONParser;
+import org.apache.storm.shade.org.json.simple.parser.ParseException;
+import org.apache.storm.task.OutputCollector;
+import org.apache.storm.task.TopologyContext;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.base.BaseRichBolt;
+import org.apache.storm.tuple.Fields;
+import org.apache.storm.tuple.Tuple;
+import org.apache.storm.tuple.Values;
+import org.dmg.pmml.FieldName;
+import org.jpmml.evaluator.Evaluator;
+import org.jpmml.evaluator.EvaluatorUtil;
+import org.jpmml.evaluator.FieldValue;
+
+import org.apache.storm.mqtt.examples.MqttBrokerPublisher;
+
+public class PMMLBolt extends BaseRichBolt{
+	private ConcurrentHashMap<String, Integer> processing;
+	
+	private Evaluator evaluator = null;
+
+	private OutputCollector collector = null;
+
+        private static MqttBrokerPublisher mqttservice = null;
+
+	public PMMLBolt(Evaluator evaluator){
+		setEvaluator(evaluator);
+	}
+
+	@Override
+	public void prepare(Map configuration, TopologyContext context, OutputCollector collector){
+		mqttservice = new MqttBrokerPublisher();
+                setCollector(collector);
+		this.processing = new ConcurrentHashMap<String, Integer>();
+                try {
+                    mqttservice.startPublisher("storm-mqtt-cid");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+	}
+/*
+	@Override
+	public void execute(Tuple tuple){
+		Evaluator evaluator = getEvaluator();
+
+		Map<FieldName, FieldValue> arguments = new LinkedHashMap<>();
+
+		List<FieldName> activeFields = evaluator.getActiveFields();
+		for(FieldName activeField : activeFields){
+			FieldValue value = EvaluatorUtil.prepare(evaluator, activeField, tuple.getValueByField(activeField.getValue()));
+
+			arguments.put(activeField, value);
+		}
+
+		Map<FieldName, ?> result = evaluator.evaluate(arguments);
+
+		Values values = new Values();
+
+		List<FieldName> targetFields = evaluator.getTargetFields();
+		for(FieldName targetField : targetFields){
+			Object targetValue = result.get(targetField);
+
+			values.add(EvaluatorUtil.decode(targetValue));
+		}
+
+		List<FieldName> outputFields = evaluator.getOutputFields();
+		for(FieldName outputField : outputFields){
+			Object outputValue = result.get(outputField);
+
+			values.add(outputValue);
+		}
+
+		OutputCollector collector = getCollector();
+
+		collector.emit(tuple, values);
+		collector.ack(tuple);
+	}
+*/
+	@Override
+    public void execute(Tuple tuple) {
+        Evaluator evaluator = getEvaluator();
+	OutputCollector collector = getCollector();
+
+        String sentence = tuple.getString(0);
+        String[] words = sentence.split("\t");
+		
+        String process_key = null;
+        JSONParser parser = new JSONParser();
+        try{
+            Object obj = parser.parse(words[1]);
+            JSONArray array = (JSONArray)obj;
+            JSONObject jobjr = new JSONObject();
+            //JSONArray result_array = new JSONArray();
+            JSONObject results = new JSONObject();
+            //System.out.println(words[1]);
+            Values values = new Values();
+            values.add(words[0]);
+
+            for (int i=0; i < array.size(); i++) {
+                JSONObject jobj = (JSONObject)array.get(i);
+                Map<FieldName, FieldValue> arguments = new LinkedHashMap<>();
+
+                // get startTime
+                if (i == 0) {
+                	results.put("ts", jobj.get("startTime"));
+                	process_key = new String(words[0]+(jobj.get("startTime")).toString());
+                	this.processing.put(process_key, 0);
+                }
+                    //jobjr.put("apnea", jobj.get("label"));
+                    results.put("apnea", jobj.get("label"));
+    		    List<FieldName> activeFields = evaluator.getActiveFields();
+    		    for (FieldName activeField : activeFields){
+    		        FieldValue value = EvaluatorUtil.prepare(evaluator, activeField, jobj.get(activeField.getValue()));
+
+    		        arguments.put(activeField, value);
+                        //jobjr.put(activeField, Math.round((Double)jobj.get(activeField.getValue())*1000)/1000d);
+                        results.put(activeField, Math.round((Double)jobj.get(activeField.getValue())*1000)/1000d);
+    		    }
+
+        		Map<FieldName, ?> result = evaluator.evaluate(arguments);
+
+            	        //JSONObject jobjr = new JSONObject();
+
+        		List<FieldName> targetFields = evaluator.getTargetFields();
+        		for(FieldName targetField : targetFields){
+        			Object targetValue = result.get(targetField);
+                                //jobjr.put("predicted", EvaluatorUtil.decode(targetValue));
+                                results.put("predicted", EvaluatorUtil.decode(targetValue)); 
+        		//	jobjr.put(targetField, EvaluatorUtil.decode(targetValue));
+        		}
+
+        		List<FieldName> outputFields = evaluator.getOutputFields();
+        		for(FieldName outputField : outputFields){
+        			Object outputValue = result.get(outputField);
+        			//jobjr.put(outputField, Math.round((Double)outputValue*100)/100.0d);
+                                results.put(outputField, Math.round((Double)outputValue*100)/100.0d);
+        		}
+
+        		//result_array.add(jobjr);
+            }
+ 
+            //results.put("values", jobjr); //thingsboard format
+            values.add(results.toJSONString());
+           
+            //System.out.println(results.toJSONString());
+            if (this.processing.get(process_key) == 0) {
+    		   collector.emit(tuple, values);
+    		   collector.ack(tuple);
+    		   this.processing.put(process_key, this.processing.get(process_key) + 1);
+                   try{
+                       //mqttservice.publish("", results.toJSONString());
+                       mqttservice.publish("sensor/"+words[0]+"/hrv", results.toJSONString());
+                   }
+                   catch (Exception e)
+                   {
+                       System.out.println("Exception occurred");
+                   }
+            }
+         }catch(ParseException pe){
+            collector.ack(tuple);
+            System.out.println("position: " + pe.getPosition());
+            System.out.println(pe);
+         }
+         catch(Exception pe){
+            collector.ack(tuple);
+            System.out.println(pe);
+         }
+
+
+         //collector.ack(tuple);
+    }
+    
+	@Override
+	public void cleanup(){
+		super.cleanup();
+	}
+
+/*	@Override
+	public void declareOutputFields(OutputFieldsDeclarer declarer){
+		Evaluator evaluator = getEvaluator();
+
+		List<String> fields = new ArrayList<>();
+
+		List<FieldName> targetFields = evaluator.getTargetFields();
+		for(FieldName targetField : targetFields){
+			fields.add(targetField.getValue());
+		}
+
+		List<FieldName> outputFields = evaluator.getOutputFields();
+		for(FieldName outputField : outputFields){
+			fields.add(outputField.getValue());
+		}
+
+		declarer.declare(new Fields(fields));
+	}
+*/
+	@Override
+	public void declareOutputFields(OutputFieldsDeclarer declarer){
+		Fields hdfsFields = new Fields("subject", "apnea");
+		declarer.declare(hdfsFields);
+	}
+	
+	public Evaluator getEvaluator(){
+		return this.evaluator;
+	}
+
+	private void setEvaluator(Evaluator evaluator){
+		this.evaluator = evaluator;
+	}
+
+	public OutputCollector getCollector(){
+		return this.collector;
+	}
+
+	private void setCollector(OutputCollector collector){
+		this.collector = collector;
+	}
+}
